dce2b2835326d8174465b9451c0e20705b8240c8
Thiago <47746589+ThiagoDeAM@users.noreply.github.com>
2025-06-11 13:52:37 -0300

    feat: create ChatClient

---
+++ ChatClient.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ChatClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket(SERVER_ADDRESS, PORT);
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+        new Thread(() -> {
+            try {
+                String msg;
+                while((msg = input.readLine()) != null) {
+                    System.out.println(msg);
+                }
+            } catch (IOException e) {
+                System.out.println("Conexão encerrada.");
+            }
+        }).start();
+
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        String userMsg;
+        while ((userMsg = userInput.readLine()) != null) {
+            output.println(userMsg);
+        }
+    }
+}
